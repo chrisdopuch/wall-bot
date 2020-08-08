64fa7a912eb5061564b2980be98aa87da5bfb941
Chris Dopuch <chris.dopuch@gmail.com>
2020-08-07 18:43:14 -0700

    prefix polls module with server in settings

--- server/server/settings.py
+++ server/server/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
+    'server.polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
